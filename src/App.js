11fe6c31e507d19605a2c0301bc89a3e28c120d8
Sarang Raotole <sarang61188@gmail.com>
2022-07-03 22:35:08 +0530

    firebase integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
+let isFirstRender = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
-  const cartItems=useSelector((state)=>state.cart.itemsList);
-  console.log(cartItems);
+  useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+    const sendRequest = async () => {
+      //Send state as Sending Request
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending Request",
+          type: "warning",
+        })
+      );
+      const res = await fetch(
+        "https://redux-http-f5f2c-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+      //Send state as Request successful
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sent Request To DB Successfully",
+          type: "success",
+        })
+      );
+    };
+    sendRequest().catch((err) => {
+      //Send state as Error
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending Request Failed",
+          type: "error",
+        })
+      );
+    });
+  }, [cart]);
   return (
     <div className="App">
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
